63a51227a6a95d8519108bb2b0ebf723f5aefa09
Mugu T <ttmugu@gmail.com>
2019-10-17 02:55:09 -0400

    PyPoll final
    result file will created on PyPoll folder
    text file will append result evry time program run

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,14 @@
 import os
 import csv
 
-resultdic ={
-    "Total_Voters": 0 
-}
+total_voters =  0 
+resultdic ={}
+
+#funtion that will call below for write result
+def write_to_file(text):
+    output_file = os.path.join('Election Results.txt')
+    with open(output_file, 'a',) as txtfile:
+        txtfile.write(text)
 
 
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -13,23 +18,36 @@ with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     for row in csvreader:
-        resultdic["Total_Voters"] += 1
+        total_voters += 1
         if row[2] not in resultdic:
             resultdic[row[2]] = 1
         if row[2] in resultdic:
             resultdic[row[2]] += 1
-
-print(resultdic)
+# print in screen
+#print(resultdic)
 print("Election Results")
+write_to_file("Election Results\n")
 print("-------------------------")
-#print(f"{resultdec.keys().index(0)} : {resultdec.keys().index(0)[0]}")
-# need to print result correct format 
-for key, value in resultdic.items(): 
-    print (key, value) 
+write_to_file("-------------------------\n")
+print(f"Total Voters: {total_voters}")
+write_to_file(f"Total Voters: {total_voters}\n")
+print("-------------------------")
+write_to_file("-------------------------\n")
+
+for key in resultdic.keys(): 
+    value = resultdic[key]
+    percent = round(100 * (resultdic[key]/total_voters), 3)
+    print (f"{key}: {percent}% ({value})") 
+    write_to_file(f"{key}: {percent}% ({value})\n")
 
 print("-------------------------")
-# need to print winner here
+write_to_file("-------------------------\n")
+key = max(resultdic, key = lambda k: resultdic[k])
+print(f"Winner : {key}")
+write_to_file(f"Winner : {key}\n")
 print("-------------------------")
+write_to_file("-------------------------\n")
+
